7e9575cf0c8e25b9c0d6d9c8a1a488f0ead68011
suwaroj-se <suwaroj.se@gmail.com>
2022-12-28 11:56:31 +0700

    [EXP01] - improve create_test in unit test 1

--- expense/create_test.go
+++ expense/create_test.go
@@ -1,6 +1,7 @@
 package expense
 
 import (
+	"encoding/json"
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
@@ -33,12 +34,29 @@ func TestCreateExpense(t *testing.T) {
 	t.Run("Create expenses succecss", func(t *testing.T) {
 		mock.ExpectQuery("INSERT INTO expenses").WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
 		con := Conn{db}
+		var w, g Expenses
+		// want := `{"id":1,"title":"strawberry smoothie","amount":79,"note":"night market promotion discount 10 bath","tags":["food","beverage"]}` + "\n"
+		want := `{
+			"id": 1,
+			"title": "strawberry smoothie",
+			"amount": 79,
+			"note": "night market promotion discount 10 bath", 
+			"tags": ["food", "beverage"]
+		}`
 
-		want := `{"id":1,"title":"strawberry smoothie","amount":79,"note":"night market promotion discount 10 bath","tags":["food","beverage"]}` + "\n"
+		err := json.Unmarshal([]byte(want), &w)
+		if err != nil {
+			t.Fatal("Error convert 'want' with json.Unmarshall", err)
+		}
 
 		if assert.NoError(t, con.CreateExpenseHadler(c)) {
+			err = json.Unmarshal(rec.Body.Bytes(), &g)
+			if err != nil {
+				t.Fatal("Error convert 'rec.Body.Bytes' with json.Unmarshall", err)
+			}
+
 			assert.Equal(t, http.StatusCreated, rec.Code)
-			assert.Equal(t, want, rec.Body.String())
+			assert.Equal(t, w, g)
 		}
 	})
 }
--- go.mod
+++ go.mod
@@ -4,18 +4,20 @@ go 1.19
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0
-	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.9.1
 	github.com/lib/pq v1.10.7
 	github.com/stretchr/testify v1.8.1
+	gotest.tools v2.2.0+incompatible
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
