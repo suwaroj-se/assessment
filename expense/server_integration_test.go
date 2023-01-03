2a571aef13bddd3aee9c0c4629738aa1cf54bc95
suwaroj-se <suwaroj.se@gmail.com>
2023-01-03 16:30:05 +0700

    add server_integration_test

--- expense/server_integration_test.go
+++ expense/server_integration_test.go
@@ -14,6 +14,130 @@ import (
 	"testing"
 )
 
-func TestCreateExpense(t *testing.T) {
+// [EXP01]
+func TestIntegrationCreateExpense(t *testing.T) {
+	body := bytes.NewBufferString(`{
+		"title": "strawberry smoothie",
+		"amount": 79,
+		"note": "night market promotion discount 10 bath", 
+		"tags": ["food", "beverage"]
+	}`)
 
+	var ex Expense
+
+	res := request(http.MethodPost, uri("expenses"), body)
+	err := res.Decode(&ex)
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusCreated, res.StatusCode)
+	assert.NotEqual(t, 0, ex.ID)
+	assert.Equal(t, "strawberry smoothie", ex.Title)
+	assert.Equal(t, 79.0, ex.Amount)
+	assert.Equal(t, "night market promotion discount 10 bath", ex.Note)
+	assert.Equal(t, []string{"food", "beverage"}, ex.Tags)
+
+}
+
+// [EXP02]
+func TestGetExpensesByID(t *testing.T) {
+	c := seedExpense(t)
+
+	var latest Expense
+	res := request(http.MethodGet, uri("expenses", strconv.Itoa(c.ID)), nil)
+	err := res.Decode(&latest)
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+	assert.Equal(t, c.ID, latest.ID)
+	assert.NotEmpty(t, latest.Title)
+	assert.NotEmpty(t, latest.Amount)
+	assert.NotEmpty(t, latest.Note)
+	assert.NotEmpty(t, latest.Tags)
+}
+
+// [EXP03]
+func TestPutExpensesByID(t *testing.T) {
+	body := bytes.NewBufferString(`{
+		"title": "apple smoothie",
+		"amount": 89,
+		"note": "no discount",
+		"tags": ["beverage"]
+	}`)
+
+	c := seedExpense(t)
+
+	var latest Expense
+	res := request(http.MethodPut, uri("expenses", strconv.Itoa(c.ID)), body)
+	err := res.Decode(&latest)
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+	assert.Equal(t, c.ID, latest.ID)
+	assert.NotEmpty(t, latest.Title)
+	assert.NotEmpty(t, latest.Amount)
+	assert.NotEmpty(t, latest.Note)
+	assert.NotEmpty(t, latest.Tags)
+}
+
+// [EXP04]
+func TestGetAllExpense(t *testing.T) {
+	seedExpense(t)
+
+	var ex []Expense
+	res := request(http.MethodGet, uri("expenses"), nil)
+	err := res.Decode(&ex)
+
+	assert.Nil(t, err)
+	assert.EqualValues(t, http.StatusOK, res.StatusCode)
+	assert.Greater(t, len(ex), 0)
+
+}
+
+func seedExpense(t *testing.T) Expense {
+	var c Expense
+	body := bytes.NewBufferString(`{
+		"title": "strawberry smoothie",
+		"amount": 79,
+		"note": "night market promotion discount 10 bath", 
+		"tags": ["food", "beverage"]
+	}`)
+
+	err := request(http.MethodPost, uri("expenses"), body).Decode(&c)
+	if err != nil {
+		t.Fatal("can't create expense:", err)
+	}
+
+	return c
+}
+
+func uri(paths ...string) string {
+	host := "http://localhost:2565"
+	if paths == nil {
+		return host
+	}
+
+	url := append([]string{host}, paths...)
+	return strings.Join(url, "/")
+}
+
+type Response struct {
+	*http.Response
+	err error
+}
+
+func (r *Response) Decode(v interface{}) error {
+	if r.err != nil {
+		return r.err
+	}
+
+	return json.NewDecoder(r.Body).Decode(v)
+}
+
+func request(method, url string, body io.Reader) *Response {
+	req, _ := http.NewRequest(method, url, body)
+	req.Header.Add("Authorization", os.Getenv("AUTH_TOKEN"))
+	req.Header.Add("Content-Type", "application/json")
+	client := http.Client{}
+	res, err := client.Do(req)
+	return &Response{res, err}
 }
